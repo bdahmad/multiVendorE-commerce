1f84b1ca386ed5855e1c873676cb15fe0b6a3198
Ahmad Ali <ahmadali09787@gmail.com>
2023-10-19 00:01:58 +0600

    108 fake data & seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,15 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'photo' => fake()->imageUrl('60', '60'),
+            'phone' => fake()->phoneNumber,
+            'address' => fake()->address,
+            'role' => fake()->randomElement(['admin', 'vendor', 'user']),
+            'status' => fake()->randomElement(['active', 'inactive']),
             'remember_token' => Str::random(10),
         ];
     }
