6fa55ecb380647d9c0dab73649be0fd503fa6b66
Ahmad Ali <ahmadali09787@gmail.com>
2023-11-03 19:12:58 +0600

    forgot password page setup

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,42 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@extends('layouts.frontend')
+@section('content')
+<div class="page-header breadcrumb-wrap">
+    <div class="container">
+        <div class="breadcrumb">
+            <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
+            <span></span> Pages <span></span> My Account
+        </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+</div>
+<div class="page-content pt-150 pb-150">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-8 col-md-12 m-auto">
+                <div class="row">
+                    <div class="heading_s1">
+                        <img class="border-radius-15" src="{{asset('frontend')}}/assets/imgs/page/reset_password.svg" alt="" />
+                        <h2 class="mb-15 mt-15">Email Password Reset Link</h2>
+                        <p class="mb-30">Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</p>
+                    </div>
+                    <div class="col-lg-6 col-md-8">
+                        <div class="login_wrap widget-taber-content background-white">
+                            <div class="padding_eight_all bg-white">
+                                <form method="POST" action="{{ route('password.email') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <input type="email" id="email" required="" name="email" placeholder="Enter email address" />
+                                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-heading btn-block hover-up" name="login">Email Password Reset Link</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</div>
+@endsection
