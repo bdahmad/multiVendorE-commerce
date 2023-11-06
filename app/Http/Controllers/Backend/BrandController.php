6641e7ecd0df4ef1a309abf9b2661c67594fa86d
Ahmad Ali <ahmadali09787@gmail.com>
2023-11-06 23:37:12 +0600

    brand page 2

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Backend\Brand;
 
 class BrandController extends Controller
 {
     public function index()
     {
-        return view('admin.brand.all');
+        $all = Brand::where('brand_status', 1)->get();
+        return view('admin.brand.all', compact('all'));
     }
     public function add()
     {
--- resources/views/admin/brand/all.blade.php
+++ resources/views/admin/brand/all.blade.php
@@ -1,4 +1,64 @@
 @extends('layouts.adminbackend')
 @section('content')
+<div class="page-content">
+    <!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+        <div class="breadcrumb-title pe-3">Brand</div>
+        <div class="ps-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 p-0">
+                    <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">All Brand</li>
+                </ol>
+            </nav>
+        </div>
+        <div class="ms-auto">
+            <div class="btn-group">
+                <a type="button" href="{{route('add-brand')}}" class="btn btn-primary">Add Brand</a>
+            </div>
+        </div>
+    </div>
+    <!--end breadcrumb-->
+    <!-- <h6 class="mb-0 text-uppercase">DataTable Example</h6> -->
+    <hr />
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="example" class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>SI</th>
+                            <th>Name</th>
+                            <th>Image</th>
+                            <th>Manage</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($all as $key => $data)
+                        <tr>
+                            <td>$key+1</td>
+                            <td>$data->brand_name</td>
+                            <td>$data->brand_image</td>
+                            <td>
+                                <a href="#"><i class="fa fa-edit"></i></a>
+                                <a href="#"><i class="fa fa-trash"></i></a>
+                            </td>
+                        </tr>
+                        @endforeach
 
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>SI</th>
+                            <th>Name</th>
+                            <th>Image</th>
+                            <th>Manage</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
